a0595dd681ed83ac30f36f99f3454899265cb0d8
TurianKoYoT <dimadegalevich@yandex.ru>
2018-03-04 14:45:15 +0500

    Lesson 6 Create answer through ajax

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,12 +4,7 @@ class AnswersController < ApplicationController
   before_action :load_answer, only: [ :destroy ]
   
   def create
-    @answer = @question.answers.build(answer_params.merge(user: current_user) )
-    if @answer.save
-      redirect_to @question, notice: t('.successfull')
-    else
-      render "questions/show"
-    end
+    @answer = @question.answers.create(answer_params.merge(user: current_user) )
   end
   
   def destroy
